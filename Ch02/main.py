01400c7812d6386453e0a223d9196de5b0ff278d
gliu <gliu@gliu.lan.appannie.com>
2015-12-02 20:44:16 +0800

    add main file

---
+++ Ch02/main.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 16, 2010
+kNN: k Nearest Neighbors
+
+Input:      inX: vector to compare to existing dataset (1xN)
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison (should be an odd number)
+            
+Output:     the most popular class label
+
+@author: lingo.blyde
+'''
+import kNN
+
+group, labels = kNN.createDataSet()
+print kNN.classify0([1,5], group, labels, 3)
